c4d878fd7fee612d0f177406b331cbdcf8c085fc
Jackson Roberts <cvrgaming@gmail.com>
2021-11-22 13:59:07 -0800

    Moved YuController to correct folder and started PlayerController

--- Assets/Scripts/Player_Control/PlayerController.cs
+++ Assets/Scripts/Player_Control/PlayerController.cs
@@ -1,18 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+namespace Player_Control {
+	[RequireComponent(typeof(CharacterController))]
+	public class PlayerController : MonoBehaviour {
+		public CharacterController characterController;
+		
+		void Start() { }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+		void Update() { }
+	}
 }
