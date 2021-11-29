118c466595ad80416c1d88cd924ddaf4999863ae
TheCostco <56370779+EmersonCoskey@users.noreply.github.com>
2021-11-29 13:14:15 -0800

    completed stuff
    updated .gitignore to include .DS_Store

--- Assets/Scripts/Other/DummyComponent.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-namespace Other {
-	public class DummyComponent : MonoBehaviour {
-		[SerializeField] private Optional<int> test = new Optional<int>(123, true);
-		[SerializeField] private int           amogus;
-	}
-}
