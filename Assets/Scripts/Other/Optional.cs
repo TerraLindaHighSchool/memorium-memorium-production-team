714a06fca5b3fea5deaaa3c56f4787a126c34310
TheCostco <56370779+EmersonCoskey@users.noreply.github.com>
2021-12-07 13:35:07 -0800

    lots of comments

--- Assets/Scripts/Other/Optional.cs
+++ Assets/Scripts/Other/Optional.cs
@@ -1,11 +1,22 @@
 using UnityEngine;
 
 namespace Other {
+	/// <summary>
+	/// A value that may or may not exist/be relevant. Also an immutable data structure.
+	/// </summary>
+	/// <typeparam name="T">The type of the contained value.</typeparam>
 	public struct Optional<T> {
 		[SerializeField] private T _value;
 		[SerializeField] private bool _enabled;
 
+		/// <summary>
+		/// the contained value.
+		/// </summary>
 		public T Value => _value;
+		
+		/// <summary>
+		/// if the contained value is relevant.
+		/// </summary>
 		public bool Enabled => _enabled;
 
 		public Optional(T value, bool enabled) {
--- Assets/Scripts/Puzzle_Control/PuzzleController.cs
+++ Assets/Scripts/Puzzle_Control/PuzzleController.cs
@@ -3,19 +3,61 @@ using UnityEngine;
 using UnityEngine.Events;
 
 namespace Puzzle_Control {
+	/// <summary>
+	/// An abstract representation of a puzzle, with the following components:
+	/// <list type="definition">
+	///		<item>
+	///			<term>Guid</term>
+	///			<definition>a unique string that identifies this puzzle.</definition>
+	///		</item>
+	///		<item>
+	///			<term>Event Publishers</term>
+	///			<definition>
+	///				A c# event to notify puzzleSets of this puzzle's completion,
+	///				and a unity event to be used in the editor to trigger various things
+	///				upon puzzle completion.
+	///			</definition>
+	///		</item>
+	///		<item>
+	///			<term><c>StartPuzzle();</c></term>
+	///			<definition>
+	///				This is the "entrypoint" to puzzle logic, which is to be called (likely by an
+	///				Interactable) when the puzzle should begin.
+	///			</definition>
+	///		</item>
+	/// </list>
+	/// </summary>
 	public abstract class PuzzleController : MonoBehaviour {
+		
+		/// <summary>
+		/// a unique string that identifies this puzzle.
+		/// </summary>
+		public string Guid { get; private set; }
+		
+		/// <summary>
+		/// a unity event that is called when this puzzle is completed.
+		/// </summary>
 		[SerializeField] UnityEvent OnPuzzleComplete;
 
-		public string Guid { get; private set; }
-
+		/// <summary>
+		/// a c# event that is used to notify containing puzzle sets of this puzzle's completion (puzzle set
+		/// not required for puzzle to function)
+		/// </summary>
 		public event Action<string> NotifyPuzzleSet; //Tell the contained puzzle set the puzzle is complete
 		
+		/// <summary>
+		/// This is the "entrypoint" to puzzle logic, which is to be called (likely by an
+		///	Interactable) when the puzzle should begin.
+		/// </summary>
 		public abstract void StartPuzzle();
 
 		void OnEnable() {
 			Guid = System.Guid.NewGuid().ToString();
 		}
 
+		/// <summary>
+		/// call this to complete the puzzle and end puzzle context.
+		/// </summary>
 		protected void Complete() {
 			OnPuzzleComplete.Invoke();
 			NotifyPuzzleSet?.Invoke(Guid);
