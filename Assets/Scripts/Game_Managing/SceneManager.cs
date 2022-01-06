11329139218a2af1469a2da38eb8f818b19f1d54
TheCostco <56370779+EmersonCoskey@users.noreply.github.com>
2022-01-06 13:24:08 -0800

    SceneManager and SceneTransitionTarget done!

--- Assets/Scripts/Game_Managing/SceneManager.cs
+++ Assets/Scripts/Game_Managing/SceneManager.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Collections.Generic;
-using Other;
+using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Game_Managing {
-	public class SceneManager : Singleton<SceneManager> {
+	public static class SceneManager /*: Singleton<SceneManager>*/ { //TODO: remove static modifier
 		[Flags]
 		public enum Scene : byte {
+			None               = 0b_0000_0000, //don't actually use this!
 			MainMenu           = 0b_0000_0001,
 			TutorialIsland     = 0b_0000_0010,
 			FlowerIsland       = 0b_0000_0100,
@@ -13,45 +15,42 @@ namespace Game_Managing {
 			LibraryIsland      = 0b_0001_0000,
 			StainedGlassIsland = 0b_0010_0000,
 			Graveyard          = 0b_0100_0000,
-
-			MainGameplay = TutorialIsland
-			             | FlowerIsland
-			             | FeatherIsland
-			             | LibraryIsland
-			             | StainedGlassIsland
+		}
+		
+		public static string GetPath(Scene scene) {
+			throw new NotImplementedException("FILL IN THE SWITCH STATEMENT STRINGS");
+			string sceneName =
+				scene switch {
+					Scene.MainMenu           => "",
+					Scene.TutorialIsland     => "",
+					Scene.FlowerIsland       => "",
+					Scene.FeatherIsland      => "",
+					Scene.LibraryIsland      => "",
+					Scene.StainedGlassIsland => "",
+					Scene.Graveyard          => "",
+					_                        => throw new ArgumentException("Invalid Scene ID")
+				};
+			return "Gameplay_Scenes" + sceneName;
 		}
 
-		public static class SceneExtensions {
-			public static string GetPath(Scene scene) {
-				string sceneName =
-					scene switch {
-						Scene.MainMenu           => "",
-						Scene.TutorialIsland     => "",
-						Scene.FlowerIsland       => "",
-						Scene.FeatherIsland      => "",
-						Scene.LibraryIsland      => "",
-						Scene.StainedGlassIsland => "",
-						Scene.Graveyard          => "",
-						_                        => throw new ArgumentException("Invalid Scene ID")
-					};
-				return "Gameplay_Scenes" + sceneName;
+		public static string[] GetPaths(Scene scene) {
+			List<string> initialList = new List<string>();
+			foreach (Scene e in Enum.GetValues(typeof(Scene))) {
+				if ((scene & e) == e) initialList.Add(GetPath(e));
 			}
-
-			public static string[] GetPathsSafe(Scene scene) {
-				List<string> initialList = new List<string>();
-				if ((scene & Scene.MainMenu) == Scene.MainMenu) initialList.Add(GetPath(Scene.MainMenu));
-				if ((scene & Scene.TutorialIsland) == Scene.TutorialIsland) initialList.Add(GetPath(Scene.TutorialIsland));
-				if ((scene & Scene.FlowerIsland)  == Scene.FlowerIsland) initialList.Add(GetPath(Scene.FlowerIsland));
-				if ((scene & Scene.FeatherIsland) == Scene.FeatherIsland) initialList.Add(GetPath(Scene.FeatherIsland));
-				if ((scene & Scene.LibraryIsland) == Scene.LibraryIsland) initialList.Add(GetPath(Scene.LibraryIsland));
-				if ((scene & Scene.StainedGlassIsland) == Scene.StainedGlassIsland) initialList.Add(GetPath(Scene.StainedGlassIsland));
-				if ((scene & Scene.Graveyard) == Scene.Graveyard) initialList.Add(GetPath(Scene.Graveyard));
-				return initialList.ToArray();
-			}
+			return initialList.ToArray();
 		}
 
-		public void Load(Scene scene) {
-			string[] paths = scene.GetPathsSafe()
+		public static void Load(Scene scene) {
+			string[] paths = GetPaths(scene);
+			int      index = Random.Range(0, paths.Length - 1);
+			UnityEngine.SceneManagement.SceneManager.LoadScene(paths[index]);
+		}
+		
+		public static AsyncOperation LoadAsync(Scene scene) {
+			string[] paths = GetPaths(scene);
+			int      index = Random.Range(0, paths.Length - 1);
+			return UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(paths[index]);
 		}
 	}
 }
