b7e052c8c47b3d1189e34b5728ed79c8d707fe88
Jackson Roberts <cvrgaming@gmail.com>
2022-01-19 20:24:22 -0800

    Continued work on GameContext
    Yes I know that this is technically not my task but bebebobe

--- Assets/Scripts/Game_Managing/Game_Context/IGameContext.cs
+++ Assets/Scripts/Game_Managing/Game_Context/IGameContext.cs
@@ -1,11 +1,13 @@
 using System;
-using UnityEngine;
 using Cinemachine;
+using UnityEngine;
 
-namespace Game_Managing {
-	public abstract class GameContext {
+namespace Game_Managing.Game_Context {
+	public interface IGameContext {
 		public abstract CinemachineVirtualCamera VCam { get; }
 
+		public abstract void GCStart();
+		
 		public abstract void GCUpdate(Vector2 mouseDelta, bool rcDown);
 
 		public event Action onExit;
