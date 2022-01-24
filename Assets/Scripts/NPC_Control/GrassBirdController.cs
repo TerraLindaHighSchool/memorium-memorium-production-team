32c26d045f80c4111165638e8ef424fcd600fe4a
Jackson Roberts <cvrgaming@gmail.com>
2022-01-24 13:22:40 -0800

    Finished Grass Bird Prefab
    it's in Assets/Prefabs/Character/

--- Assets/Scripts/NPC_Control/GrassBirdController.cs
+++ Assets/Scripts/NPC_Control/GrassBirdController.cs
@@ -1,39 +1,34 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class GrassBirdController : MonoBehaviour
-{
-    public EntityController entityController;
+namespace NPC_Control {
+	[RequireComponent(typeof(EntityController))]
+	public class GrassBirdController : MonoBehaviour {
+		public EntityController entityController;
 
-    [SerializeField] private Vector3 origin;
-    [SerializeField] private Vector3 boxSize = new Vector3(5,5,5);
-    [SerializeField] private bool lockOnYAxis = true;
+		[SerializeField] private Vector3 origin;
+		[SerializeField] private Vector3 boxSize     = new Vector3(5, 5, 5);
+		[SerializeField] private bool    lockOnYAxis = true;
 
-    [SerializeField] private float maxWait = 10;
-    [SerializeField] private float minWait = 2;
+		[SerializeField] private float maxWait = 10;
+		[SerializeField] private float minWait = 2;
 
-    private bool canMove = true;
+		private bool canMove = true;
 
-    private void Start()
-    {
-        StartCoroutine(MoveToRandom());
-    }
+		private void Start() { StartCoroutine(MoveToRandom()); }
 
-    private Vector3 getRandomPoint()
-    {
-        float x = Random.Range(-boxSize.x, boxSize.x);
-        float y = lockOnYAxis ? 0 : Random.Range(-boxSize.y, boxSize.y);
-        float z = Random.Range(-boxSize.z, boxSize.z);
-        return new Vector3(x, y, z) + origin;
-    }
+		private Vector3 GetRandomPoint() {
+			float x = Random.Range(-boxSize.x, boxSize.x);
+			float y = lockOnYAxis ? 0 : Random.Range(-boxSize.y, boxSize.y);
+			float z = Random.Range(-boxSize.z, boxSize.z);
+			return new Vector3(x, y, z) + origin;
+		}
 
-    private IEnumerator MoveToRandom()
-    {
-        while (canMove)
-        {
-            entityController.MoveToPoint(getRandomPoint());
-            yield return new WaitForSeconds(Random.Range(minWait, maxWait));
-        }
-    }
+		private IEnumerator MoveToRandom() {
+			while (canMove) {
+				entityController.MoveToPoint(GetRandomPoint());
+				yield return new WaitForSeconds(Random.Range(minWait, maxWait));
+			}
+		}
+	}
 }
