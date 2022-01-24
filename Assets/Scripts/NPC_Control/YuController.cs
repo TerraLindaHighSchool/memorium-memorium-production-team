17fc2f3747a0938645a0a6c24b6ddb9b9d891b08
WoofMeister <56370697+DillonSumnerEpicGamer@users.noreply.github.com>
2022-01-24 13:35:55 -0800

    circle

--- Assets/Scripts/NPC_Control/YuController.cs
+++ Assets/Scripts/NPC_Control/YuController.cs
@@ -7,6 +7,8 @@ public class YuController : MonoBehaviour
     public Transform Player;
 
     [SerializeField] private float radius = 2;
+    [SerializeField] private int circle_steps = 10;
+    [SerializeField] private int y;
     [SerializeField] private float speed = 0.1f;
 
     private void Update()
@@ -27,6 +29,15 @@ public class YuController : MonoBehaviour
     }
     private void MoveAroundPlayer()
     {
+        float circle = 2 * Mathf.PI;
+        
+        for (int i = 1; i == circle_steps; i ++)
+        {
+            float angle = i * (circle / circle_steps);
+            float x = Mathf.Cos(angle) * radius;
+            float z = Mathf.Sin(angle) * radius;
 
+            Vector3 position = new Vector3(x, y, z);
+        }
     }
 }
