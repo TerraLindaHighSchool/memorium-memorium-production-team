622efd2e6bd9f030f001ed0049efea94b05b6310
WoofMeister <56370697+DillonSumnerEpicGamer@users.noreply.github.com>
2022-01-27 13:32:50 -0800

    it works like ur mom does when ur mom is a a mom that goes to work #gotem

--- Assets/Scripts/NPC_Control/YuController.cs
+++ Assets/Scripts/NPC_Control/YuController.cs
@@ -7,24 +7,40 @@ public class YuController : MonoBehaviour
     public Transform Player;
 
     [SerializeField] private float radius = 5;
-    [SerializeField] private float speed = 0.1f;
+    [SerializeField] private float sine_force = 1;
+    [SerializeField] private float speed = 0.01f;
+    [SerializeField] private float yOffset = 10;
+
+    private int degree = 0;
 
     private bool canFloat = true;
 
     private void Update()
     {
+        
         if (Vector3.Distance(Player.position, this.transform.position) >= radius / 2)
         {
-            MoveToPlayer();
+            this.transform.position = MoveToPlayer();
         }
         else
         {
-            this.transform.position = Vector3.Lerp(this.transform.position + new Vector3(0,0,radius/2), Player.position, speed);
+            this.transform.position = RotateAroundPlayer();
         }
     }
 
-    private void MoveToPlayer()
+    private Vector3 MoveToPlayer()
     {
-        this.transform.position = Vector3.Lerp(this.transform.position, Player.position, speed);
+        return Vector3.Lerp(this.transform.position, Player.position, speed);
+    }
+    private Vector3 RotateAroundPlayer()
+    {
+        degree++;
+        float rad = degree * Mathf.Deg2Rad;
+
+        float x = Mathf.Cos(rad) * radius;
+        float y = (Mathf.Sin(rad) * sine_force);
+        float z = Mathf.Sin(rad) * radius;
+
+        return Vector3.Lerp(this.transform.position, new Vector3(x, y, z) + Player.position, speed);
     }
 }
