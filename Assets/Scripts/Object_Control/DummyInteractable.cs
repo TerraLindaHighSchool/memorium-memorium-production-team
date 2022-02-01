0027be9c738f86e95f186f9c83274ce334a0554c
ZachRocco <60672549+ZachRocco@users.noreply.github.com>
2022-02-01 12:58:45 -0800

    trying to fix this broken branch :(

---
+++ Assets/Scripts/Object_Control/DummyInteractable.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Object_Control {
+	/// <summary>
+	/// Just a dummy Interactable controller.
+	/// Meant to be used as an example or as a test to ensure interactions are working.
+	/// </summary>
+	public class DummyInteractable : MonoBehaviour {
+		/// <summary>
+		/// Dummy interact method.
+		/// Just logs a message to the console.
+		/// </summary>
+		public void Interact() { Debug.Log($"Testing Interactable Object {this}"); }
+	}
+}
