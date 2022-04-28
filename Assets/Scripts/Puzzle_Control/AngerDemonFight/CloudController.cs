ad5835dd5eb0f945d9bee4a7f981fbe0b87c5060
Niko Chow-Stuart <nc01843@srcs.org>
2022-04-28 14:06:30 -0700

    small changes

--- Assets/Scripts/Puzzle_Control/AngerDemonFight/CloudController.cs
+++ Assets/Scripts/Puzzle_Control/AngerDemonFight/CloudController.cs
@@ -4,16 +4,31 @@ using UnityEngine;
 
 public class CloudController : MonoBehaviour
 {
+
+	Animator animator;
+
+	void Start() {
+		animator = GetComponent<Animator>();
+		
+		Birth();
+	}
+
+	void Birth() {
+		animator.SetTrigger("Birth");
+	}
+
 	void FixedUpdate()
 	{
 		RaycastHit hit;
 		if (Physics.Raycast(transform.position, Vector3.down, out hit, 100.0f))
 		{
 			// if the raycast hits an object with method "TouchRain", call it
+			/*
 			if (hit.collider.gameObject.GetComponent<ReplaceMe>() != null)
 			{
 				hit.collider.gameObject.GetComponent<ReplaceMe>().TouchRain();
 			}
+			*/
 		}
 	}
 }
