3a32d7ff25d5d27c671befde667f516b549b5917
Niko Chow-Stuart <nc01843@srcs.org>
2022-04-28 13:56:59 -0700

    cloud state machine

---
+++ Assets/Scripts/Puzzle_Control/AngerDemonFight/CloudController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudController : MonoBehaviour
+{
+	void FixedUpdate()
+	{
+		RaycastHit hit;
+		if (Physics.Raycast(transform.position, Vector3.down, out hit, 100.0f))
+		{
+			// if the raycast hits an object with method "TouchRain", call it
+			if (hit.collider.gameObject.GetComponent<ReplaceMe>() != null)
+			{
+				hit.collider.gameObject.GetComponent<ReplaceMe>().TouchRain();
+			}
+		}
+	}
+}
