18a3b9cb26f8e21f5d67de7c2a13017675ae27d3
Niko Chow-Stuart <nc01843@srcs.org>
2022-05-09 13:33:11 -0700

    added demon to test scene and autokilling clouds

--- Assets/Scripts/Puzzle_Control/AngerDemonFight/CloudController.cs
+++ Assets/Scripts/Puzzle_Control/AngerDemonFight/CloudController.cs
@@ -7,6 +7,10 @@ public class CloudController : MonoBehaviour
 
 	Animator animator;
 
+	// Amount of time this cloud has been alive
+	private float timeAlive = 0;
+	private bool dying = false;
+
 	void Start() {
 		animator = GetComponent<Animator>();
 		
@@ -15,10 +19,28 @@ public class CloudController : MonoBehaviour
 
 	void Birth() {
 		animator.SetTrigger("Birth");
+		timeAlive = 0;
+	}
+
+	void Death() {
+		animator.SetTrigger("KillGracefully");
+
+		// we're gonna use the timeAlive float again to determine when to destroy this object
+		timeAlive = 0;
+		dying = true;
 	}
 
 	void FixedUpdate()
 	{
+		timeAlive += Time.deltaTime;
+
+		if (dying && timeAlive > 1.0f) {
+			Destroy(gameObject);
+		}
+		if (timeAlive > 5) {
+			Death();
+		}
+
 		RaycastHit hit;
 		if (Physics.Raycast(transform.position, Vector3.down, out hit, 100.0f))
 		{
