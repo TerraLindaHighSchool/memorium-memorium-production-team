120fd149f76ab930e124682168bf53e7c3dfb47b
LiteVoid <rh41292@srcs.org>
2022-05-09 13:55:54 -0700

    Changed the code for the flower gate controller
    i made it so that if you had both the feather and the library island shards that you can interact with the gate to remove it

--- Assets/Scripts/Object_Control/FlowerGateController.cs
+++ Assets/Scripts/Object_Control/FlowerGateController.cs
@@ -1,18 +1,13 @@
-﻿using Game_Managing;
-using Other;
-using Puzzle_Control;
+﻿using Other;
 using UnityEngine;
+using Game_Managing.Game_Context;
 
-namespace Object_Control
+namespace Puzzle_Control.FlowerGateController
 {
 	[RequireComponent(typeof(Interactable))]
 	public class FlowerGateController : PuzzleController
-	{
-
-		public const int SHARD_COUNT = 1; // Set to 1 for testing purposes; change to however many shards there are other than the flower island shards
+	{ 
 		public bool has_completed { get; private set; }
-
-		private int shards_collected = 0;
 
 		public override void StartPuzzle()
 		{
@@ -20,7 +15,7 @@ namespace Object_Control
 
 			Interactable interact = GetComponent<Interactable>();
 
-			if (has_completed || shards_collected < SHARD_COUNT)
+			if (has_completed || !Game_Managing.SaveManager.Instance.libraryShard && !Game_Managing.SaveManager.Instance.featherShard)
 			{
 				return;
 			}
@@ -29,23 +24,6 @@ namespace Object_Control
 			this.gameObject.SetActive(false);
 
 			interact.SetIsEnabled(false);
-
-			SaveManager.Instance.flowerGate = true;
 		}
-
-		public void add_collected_shard()
-		{
-			// on pick up shard
-
-			Interactable interact = GetComponent<Interactable>();
-
-			shards_collected++;
-			if (shards_collected >= SHARD_COUNT)
-			{
-				interact.SetIsEnabled(true);
-			}
-		}
-
-
 	}
 }
