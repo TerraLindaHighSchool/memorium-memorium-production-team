82c28d1f9316887c6ca58e010497ce843255721d
Jackson Roberts <jacksongroberts@protonmail.com>
2022-05-17 13:21:09 -0700

    Added shards to game
    They still needed to be added into scenes tho

---
+++ Assets/Scripts/Object_Control/ShardController.cs
@@ -0,0 +1,35 @@
+﻿using Game_Managing;
+using Other;
+using UnityEngine;
+
+namespace Object_Control {
+	[RequireComponent(typeof(Interactable))]
+	public class ShardController : MonoBehaviour {
+		// SHARD TYPE AND COLOR DIRECTORY
+		// BLUE = FLOWER
+		// GREEN = FEATHER
+		// PINK = LIBRARY
+		
+		public Type type;
+
+		public void Pickup() {
+			switch (type) {
+				case Type.Flower:
+					SaveManager.Instance.flowerShard = true;
+					break;
+				case Type.Feather:
+					SaveManager.Instance.featherShard = true;
+					break;
+				case Type.Library:
+					SaveManager.Instance.libraryShard = true;
+					break;
+			}
+		}
+
+		public enum Type {
+			Flower,
+			Feather,
+			Library
+		}
+	}
+}
