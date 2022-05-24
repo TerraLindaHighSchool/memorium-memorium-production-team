5c3a36cdd55e5741bcb11827e89c600482f018a3
Niko Chow-Stuart <nc01843@srcs.org>
2022-05-24 12:59:51 -0700

    added AngerDemonController script, added basics

---
+++ Assets/Scripts/Puzzle_Control/AngerDemonFight/AngerDemonController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AngerDemonController : MonoBehaviour
+{
+	/// <summary>
+	/// Stores amount of time until demon can be hit again (in seconds)
+	/// </summary>
+	private double cooldownTime = 0.0f;
+
+	/// <summary>
+	/// Stores amount of time to add to cooldownTime each time we get hit (in seconds)
+	/// </summary>
+	public double cooldownAmount = 2.0f;
+
+
+	/// <summary>
+	/// 
+
+}
--- Assets/Scripts/Puzzle_Control/AngerDemonFight/CloudController.cs
+++ Assets/Scripts/Puzzle_Control/AngerDemonFight/CloudController.cs
@@ -45,12 +45,12 @@ public class CloudController : MonoBehaviour
 		if (Physics.Raycast(transform.position, Vector3.down, out hit, 100.0f))
 		{
 			// if the raycast hits an object with method "TouchRain", call it
-			/*
-			if (hit.collider.gameObject.GetComponent<ReplaceMe>() != null)
+
+			if (hit.collider.gameObject.GetComponent<AngerDemonController>() != null)
 			{
-				hit.collider.gameObject.GetComponent<ReplaceMe>().TouchRain();
+				hit.collider.gameObject.GetComponent<AngerDemonController>().TouchRain();
 			}
-			*/
+
 		}
 	}
 }
