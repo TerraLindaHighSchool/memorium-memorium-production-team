876f9e16ebd0190b3c284dc3356ee98de14f108d
LiteVoid <rh41292@srcs.org>
2022-05-24 13:45:05 -0700

    Gate Functionality Implemented
    Got the gate to disappear once interacted with if you have both flower and feather shards

--- Assets/Scripts/Object_Control/FlowerGateController.cs
+++ Assets/Scripts/Object_Control/FlowerGateController.cs
@@ -1,51 +1,19 @@
-﻿using Game_Managing;
-using Other;
-using Puzzle_Control;
+﻿using Other;
 using UnityEngine;
+using Game_Managing.Game_Context;
+using System.Collections.Generic;
 
-namespace Object_Control
+namespace Puzzle_Control.FlowerGateController
 {
-	[RequireComponent(typeof(Interactable))]
-	public class FlowerGateController : PuzzleController
+	public class FlowerGateController : MonoBehaviour
 	{
-
-		public const int SHARD_COUNT = 1; // Set to 1 for testing purposes; change to however many shards there are other than the flower island shards
-		public bool has_completed { get; private set; }
-
-		private int shards_collected = 0;
-
-		public override void StartPuzzle()
+		public void OpenGate()
 		{
-			// on interact with cage
-
-			Interactable interact = GetComponent<Interactable>();
-
-			if (has_completed || shards_collected < SHARD_COUNT)
+			if (Game_Managing.SaveManager.Instance.libraryShard &&
+				Game_Managing.SaveManager.Instance.featherShard)
 			{
-				return;
-			}
-			has_completed = !has_completed;
-
-			this.gameObject.SetActive(false);
-
-			interact.SetIsEnabled(false);
-
-			SaveManager.Instance.flowerGate = true;
-		}
-
-		public void add_collected_shard()
-		{
-			// on pick up shard
-
-			Interactable interact = GetComponent<Interactable>();
-
-			shards_collected++;
-			if (shards_collected >= SHARD_COUNT)
-			{
-				interact.SetIsEnabled(true);
+				gameObject.SetActive(false);
 			}
 		}
-
-
 	}
 }
