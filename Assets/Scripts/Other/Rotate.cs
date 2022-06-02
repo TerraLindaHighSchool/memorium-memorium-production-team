fb38bdd4fdf8eddd7af8b6080763d1d1327b0f8d
ZachRocco <60672549+ZachRocco@users.noreply.github.com>
2022-06-02 13:05:46 -0700

    key fragment things

---
+++ Assets/Scripts/Other/Rotate.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    [SerializeField] private int speed; ///rotation speed
+	
+	
+	///rotates object, used for keys
+    void Update()
+    {
+        transform.eulerAngles += new Vector3(speed, 0, 0);
+    }
+}
